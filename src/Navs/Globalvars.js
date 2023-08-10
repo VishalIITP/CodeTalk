f89a6697af28ddf397df9b6e196949a8bd133257
Vishal Yadav <76230753+VishalIITP@users.noreply.github.com>
2023-08-10 11:09:28 +0530

    made timer time variable

--- src/Navs/Globalvars.js
+++ src/Navs/Globalvars.js
@@ -2,6 +2,6 @@ const price =299;
 const strikePrice=999;
 const wwDate="16th Aug";
 const wwTime="07:00PM";
-var timerTime=3700;    //in sec
+var timerTime=2*3600+19*60+59;  //in sec
 
 export {price, strikePrice, wwDate, wwTime, timerTime}
