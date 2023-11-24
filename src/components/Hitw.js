85df69c2ed9ea01e1e95ee002435b6ed9ebf953d
Vishal Yadav <76230753+VishalIITP@users.noreply.github.com>
2023-11-25 00:25:52 +0530

    text editing features

--- src/components/Hitw.js
+++ src/components/Hitw.js
@@ -2,59 +2,55 @@ import React from "react";
 import "./../App.css";
 import "./css/Hitw.css";
 
-function Hitw() {
+function Hitw(props) {
   return (
     <div>
       <div className="HITWORKS">
         <div className="hiwtext">
-          <span className="hiwtextwhite"> What's there in the  </span>
-          <span className="hiwtextgreen">bootcamp ?</span>
+          <span className="hiwtextwhite"> {props.actwebContent.wthc1}  </span>
+          <span className="hiwtextgreen">{props.actwebContent.wthc2}</span>
         </div>
 
         <div className="bootcampbox">
           <div className="bootcampheading">
             <div className="bchimage"></div>
-            <div className="bchtext">4 Week Bootcamp</div>
+            <div className="bchtext">{props.actwebContent.wttp}</div>
           </div>
           <div className="bootcampdisc">
             <div className="bcdlist">
               <div className="bcdltick"></div>
               <div className="bcdltext">
-                <span className="bcdHead">Flexible Course Structure:</span> We understand the demands of
-                college life, so our program is designed to fit into your busy
-                schedule. Our approach is simple: 5 percent reading concepts and 95 percent
-                practice.
+                <span className="bcdHead">{props.actwebContent.wtdp1h}:</span>{props.actwebContent.wtdp1c}
               </div>
             </div>
             <div className="bcdlist">
               <div className="bcdltick"></div>
               <div className="bcdltext">
-                <span className="bcdHead">Comprehensive Curriculum:</span> Our four-week bootcamp covers all essential DSA topics, designed to provide you with a strong foundation. From arrays to graphs, we've got you covered!
+                <span className="bcdHead">{props.actwebContent.wtdp2h}:</span> {props.actwebContent.wtdp2c}
               </div>
             </div>
             <div className="bcdlist">
               <div className="bcdltick"></div>
               <div className="bcdltext">
-                <span className="bcdHead">Live Interactive Sessions:</span> Engage in real-time discussions, problem-solving, and Q&A sessions with our mentors. Clear your doubts instantly and enhance your learning experience.
+                <span className="bcdHead">{props.actwebContent.wtdp3h}:</span> {props.actwebContent.wtdp3c}
               </div>
             </div>
             <div className="bcdlist">
               <div className="bcdltick"></div>
               <div className="bcdltext">
-                <span className="bcdHead">Weekly Coding Challenges:</span> program is designed to challenge you, to push your boundaries and
-                prepare for competitive college placement process.
+                <span className="bcdHead">{props.actwebContent.wtdp4h}:</span> {props.actwebContent.wtdp4c}
               </div>
             </div>
             <div className="bcdlist">
               <div className="bcdltick"></div>
               <div className="bcdltext">
-                <span className="bcdHead">Progress Tracking:</span> Stay on top of your growth with regular progress updates. We provide insights into your strengths and areas for improvement, ensuring focused efforts on your weaker sections.
+                <span className="bcdHead">{props.actwebContent.wtdp5h}:</span> {props.actwebContent.wtdp5c}
               </div>
             </div>
             <div className="bcdlist">
               <div className="bcdltick"></div>
               <div className="bcdltext">
-                <span className="bcdHead">Career Preparation:</span> Beyond DSA, we offer interview preparation tips and guidance on building an impressive resume. Get ready to stand out in your placements!
+                <span className="bcdHead">{props.actwebContent.wtdp6h}:</span> {props.actwebContent.wtdp6c}
               </div>
             </div>
           </div>
